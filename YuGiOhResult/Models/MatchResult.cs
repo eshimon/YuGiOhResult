bfc3b3f737b9f32ac3c040846101963a34b71f28
新也 恵下 <eshita18shin@gmail.com>
2025-04-15 10:27:55 +0900

    JSONへのセーブ、ロード処理追加

---
+++ YuGiOhResult/Models/MatchResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YuGiOhResult.Models
+{
+    class MatchResult
+    {
+        // 使用デッキ
+        public string? PlayedDeck { get; set; }
+        // 対戦相手のデッキ
+        public string? OpponentsDeck { get; set; }
+        // コインの表裏
+        public string Coin { get; set; }
+        // 勝敗
+        public string Result { get; set; }
+        // 備考
+        public string? Memo { get; set; }
+    }
+}
---
+++ YuGiOhResult/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using YuGiOhResult.Models;
+using Newtonsoft.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace YuGiOhResult.ViewModels
+{
+    partial class MainPageViewModel : ObservableObject
+    {
+        // 宣言
+        private string? filepath;
+        private MatchResult? matchResult = new MatchResult();
+        private IList<MatchResult> matches = new List<MatchResult>();
+
+        // バインディング用プロパティ宣言
+        [ObservableProperty]
+        private IList<Deck> _decks;
+        [ObservableProperty]
+        private string[] _coinList = new string[] { "表", "裏" };
+        [ObservableProperty]
+        private string _coin;
+        [ObservableProperty]
+        private string[] _turnOrderList = new string[] { "先攻", "後攻" };
+        [ObservableProperty]
+        private string _turnOrder;
+        [ObservableProperty]
+        private string[] _resultList = new string[] { "勝ち", "負け"};
+        [ObservableProperty]
+        private string _result;
+        [ObservableProperty]
+        private string? _playedDeck;
+        [ObservableProperty]
+        private string? _opponentsDeck;
+        [ObservableProperty]
+        private string? _memo;
+
+
+        public MainPageViewModel()
+        {
+            // 初期化
+            Coin = CoinList[0];
+            TurnOrder = TurnOrderList[0];
+            Result = ResultList[0];
+
+            // セーブデータ呼び出し
+            filepath = Path.Combine(FileSystem.AppDataDirectory, "savedata.json");
+            string json;
+            // セーブデータがあればロードして処理終わり
+            if (File.Exists(filepath))
+            {
+                json = File.ReadAllText(filepath);
+                matches = JsonConvert.DeserializeObject<List<MatchResult>>(json) ?? new List<MatchResult>(); //Listに変換できない
+                return;
+            }
+
+            // 空のセーブデータ(JSON)を作成
+            var emptyData = new List<MatchResult>();
+            json = System.Text.Json.JsonSerializer.Serialize(emptyData, new JsonSerializerOptions { WriteIndented = true });
+
+            // ファイルに書き込む
+            File.WriteAllText(filepath, json);
+        }
+
+        // 登録コマンド
+        [RelayCommand]
+        public void Resister()
+        {
+            MatchResult result = new MatchResult
+            {
+                PlayedDeck = this.PlayedDeck,
+                OpponentsDeck = this.OpponentsDeck,
+                Coin = this.Coin,
+                Result = this.Result,
+                Memo = this.Memo
+            };
+            matches.Add(result);
+            // JSONデータ作成
+            var options = new JsonSerializerOptions();
+            options.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
+            options.WriteIndented = true;
+            var json = System.Text.Json.JsonSerializer.Serialize(matches, options);
+            // ファイルに書き込む
+            File.WriteAllText(filepath, json);
+
+        }
+
+    }
+    
+}
